4f48f081ddbc167afadc812103cdc8c53f751e5e
Abe Voelker <_@abevoelker.com>
2020-11-09 17:09:11 -0600

    Add devise.en.yml update generator

--- lib/devise/passwordless.rb
+++ lib/devise/passwordless.rb
@@ -1,2 +1,3 @@
 require "devise/passwordless/version"
 require "devise/models/email_authenticatable"
+require "generators/devise/passwordless/install_generator"
--- lib/generators/devise/passwordless/install_generator.rb
+++ lib/generators/devise/passwordless/install_generator.rb
@@ -1,9 +1,11 @@
-require "rails/generators/named_base"
+require "rails/generators"
+require "yaml"
 
 module Devise::Passwordless
   module Generators
-    class InstallGenerator < ::Rails::Generators::NamedBase
+    class InstallGenerator < ::Rails::Generators::Base
       desc "Updates the Devise initializer to add passwordless config options"
+
       def update_devise_initializer
         inject_into_file 'config/initializers/devise.rb', before: /^end$/ do <<~'CONFIG'.indent(2)
 
@@ -18,6 +20,21 @@ module Devise::Passwordless
           # tokens. You can generate your own value with e.g. `rake secret`
           # config.passwordless_secret_key = nil
         CONFIG
+        end
+      end
+
+      def update_devise_yaml
+        devise_yaml = "config/locales/devise.en.yml"
+        begin
+          config = YAML.load_file(devise_yaml)
+        rescue Errno::ENOENT
+          STDERR.puts "Couldn't find devise.en.yml - skipping patch"
+          return
+        end
+        config["en"]["devise"]["failure"]["passwordless_invalid"] = "Invalid or expired login link."
+        config["en"]["devise"]["mailer"]["passwordless_link"] = {subject: "Your login link"}
+        File.open(devise_yaml, "w") do |f|
+          f.write(config.to_yaml)
         end
       end
     end

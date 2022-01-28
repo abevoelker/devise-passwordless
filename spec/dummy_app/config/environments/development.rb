2a1adbf6530759aa609fb7f4a65e33639e73d255
Abe Voelker <_@abevoelker.com>
2022-01-27 19:11:37 -0600

    Add devise-passwordless to dummy test app

--- spec/dummy_app/config/environments/development.rb
+++ spec/dummy_app/config/environments/development.rb
@@ -51,5 +51,4 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
 end

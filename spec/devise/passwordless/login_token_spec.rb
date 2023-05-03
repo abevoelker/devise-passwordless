001dc3689094f38ad03b4d86249def296492c4da
Justin Giancola <justin.giancola@gmail.com>
2023-05-03 18:02:27 +0000

    Add a spec

---
+++ spec/devise/passwordless/login_token_spec.rb
@@ -0,0 +1,35 @@
+require 'timecop'
+
+RSpec.describe Devise::Passwordless::LoginToken do
+  describe 'encryption and decryption' do
+    let(:user) { double(:user, email: 'email@example.com', to_key: 12345) }
+
+    before do
+      Timecop.freeze(Time.new(2023,1,1))
+
+      allow(described_class).to receive(:secret_key).and_return('sekret')
+    end
+
+    after do
+      Timecop.return
+    end
+
+    it 'can encrypt and decrypt a resource', freeze_time: Time.new(2023, 1, 1) do
+      token = described_class.encode(user)
+      decrypted = described_class.decode(token)
+
+      expected_decrypt =
+        {
+          "created_at" => 1672531200.0,
+          "data" => {
+            "resource" => {
+              "email" => "email@example.com",
+              "key" => 12345
+            }
+          }
+        }
+
+      expect(decrypted).to eq(expected_decrypt)
+    end
+  end
+end

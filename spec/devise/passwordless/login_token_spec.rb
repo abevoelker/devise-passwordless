b273cbcef720a9240ce043e4f7a0482e7407b0b0
Jim Kane <fastjames@gmail.com>
2023-05-16 10:42:34 -0500

    Use UTC time for timecop

--- spec/devise/passwordless/login_token_spec.rb
+++ spec/devise/passwordless/login_token_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Devise::Passwordless::LoginToken do
     let(:user) { double(:user, email: 'email@example.com', to_key: 12345) }
 
     before do
-      Timecop.freeze(Time.new(2023,1,1))
+      Timecop.freeze(Time.utc(2023, 1, 1))
 
       allow(described_class).to receive(:secret_key).and_return('sekret')
     end
@@ -14,7 +14,7 @@ RSpec.describe Devise::Passwordless::LoginToken do
       Timecop.return
     end
 
-    it 'can encrypt and decrypt a resource', freeze_time: Time.new(2023, 1, 1) do
+    it 'can encrypt and decrypt a resource', freeze_time: Time.utc(2023, 1, 1) do
       token = described_class.encode(user)
       decrypted = described_class.decode(token)
 
@@ -32,7 +32,7 @@ RSpec.describe Devise::Passwordless::LoginToken do
       expect(decrypted).to eq(expected_decrypt)
     end
 
-    it 'can encrypt and decrpt a resource with extra data supplied', freeze_time: Time.new(2023, 1, 1) do
+    it 'can encrypt and decrpt a resource with extra data supplied', freeze_time: Time.utc(2023, 1, 1) do
       token = described_class.encode(user, { foo: :bar })
       decrypted = described_class.decode(token)
 

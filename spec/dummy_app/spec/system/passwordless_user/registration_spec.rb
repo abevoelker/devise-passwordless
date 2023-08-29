5b12fcb80a70605f26e8e16a46c5a0d670caa7bc
Abe Voelker <_@abevoelker.com>
2023-08-29 12:20:48 -0500

    Update dummy app registration spec

--- spec/dummy_app/spec/system/passwordless_user/registration_spec.rb
+++ spec/dummy_app/spec/system/passwordless_user/registration_spec.rb
@@ -11,17 +11,14 @@ RSpec.describe "PasswordlessUser registration", :type => :system do
     expect(page.status_code).to be(200)
     expect(page).to have_css("h2", text: "Sign up")
 
+    # It creates a user in the database
     fill_in "Email", with: "foo@example.com"
-    click_button "Sign up"
+    expect { click_button "Sign up" }.to change { PasswordlessUser.count }.by(1)
 
     # It sends a confirmation email
     email = ActionMailer::Base.deliveries.find {|x|
       x.to.include?("foo@example.com")
     }
     expect(email.subject).to eq("Confirmation instructions")
-
-    # It creates a user in the database
-    expect(PasswordlessUser.count).to eq 1
-    user = PasswordlessUser.first
   end
 end

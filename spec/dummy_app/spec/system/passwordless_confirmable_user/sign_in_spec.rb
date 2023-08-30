5f02815df762336c1fd4d5a9297d9582979bed85
Abe Voelker <_@abevoelker.com>
2023-08-30 14:39:22 -0500

    Add i18n specs to dummy app

--- spec/dummy_app/spec/system/passwordless_confirmable_user/sign_in_spec.rb
+++ spec/dummy_app/spec/system/passwordless_confirmable_user/sign_in_spec.rb
@@ -64,13 +64,13 @@ RSpec.describe "PasswordlessConfirmableUser sign in", :type => :system do
 
     it "sends magic link and successfully logs in when visiting magic link" do
       visit "/passwordless_confirmable_users/sign_in"
-  
+
       expect(page.status_code).to be(200)
       expect(page).to have_css("h2", text: "Log in")
-  
+
       fill_in "Email", with: user.email
       click_button "Log in"
-  
+
       # It sends a magic link email
       mail = ActionMailer::Base.deliveries.find {|x|
         x.to.include?(email)
@@ -85,6 +85,87 @@ RSpec.describe "PasswordlessConfirmableUser sign in", :type => :system do
       # It successfully logs in
       expect(page).to have_css("h2", text: "Sign-in status")
       expect(page).to have_css("p.passwordless_confirmable_user span.email", text: user.email)
+    end
+
+    context "custom i18n" do
+      after do
+        I18n.backend.reload!
+      end
+
+      context "using global i18n options" do
+        before do
+          I18n.backend.store_translations(:en, YAML.load(
+            <<~DEVISE_I18N
+            devise:
+              passwordless:
+                magic_link_sent: "Custom magic link sent message"
+              mailer:
+                magic_link:
+                  subject: "Custom magic link message"
+            DEVISE_I18N
+          ))
+        end
+
+        it "uses the correct i18n messages" do
+          visit "/passwordless_confirmable_users/sign_in"
+
+          expect(page.status_code).to be(200)
+          expect(page).to have_css("h2", text: "Log in")
+
+          fill_in "Email", with: user.email
+          click_button "Log in"
+
+          # It displays a success message
+          expect(page).to have_text("Custom magic link sent message")
+
+          # It sends a magic link email
+          mail = ActionMailer::Base.deliveries.find {|x|
+            x.to.include?(email)
+          }
+          expect(mail.subject).to eq("Custom magic link message")
+        end
+      end
+
+      context "using resource-specific i18n options" do
+        before do
+          I18n.backend.store_translations(:en, YAML.load(
+            <<~DEVISE_I18N
+            devise:
+              passwordless:
+                passwordless_user:
+                  magic_link_sent: "YYY"
+                passwordless_confirmable_user:
+                  magic_link_sent: "Custom magic link sent message"
+                magic_link_sent: "XXX"
+              mailer:
+                magic_link:
+                  passwordless_user_subject: "YYY"
+                  passwordless_confirmable_user_subject: "Custom magic link message"
+                  subject: "XXX"
+            DEVISE_I18N
+          ))
+        end
+
+        it "uses the correct i18n messages" do
+          visit "/passwordless_confirmable_users/sign_in"
+
+          expect(page.status_code).to be(200)
+          expect(page).to have_css("h2", text: "Log in")
+
+          fill_in "Email", with: user.email
+          click_button "Log in"
+
+          # It displays a success message
+          expect(page).to have_text("Custom magic link sent message")
+
+          # It sends a magic link email
+          mail = ActionMailer::Base.deliveries.find {|x|
+            x.to.include?(email)
+          }
+          expect(mail.subject).to eq("Custom magic link message")
+        end
+      end
+
     end
   end
 end
--- spec/dummy_app/spec/system/passwordless_user/sign_in_spec.rb
+++ spec/dummy_app/spec/system/passwordless_user/sign_in_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+require "yaml"
 
 RSpec.describe "PasswordlessUser sign in", :type => :system do
   let(:email) { "foo@example.com" }
@@ -6,17 +7,48 @@ RSpec.describe "PasswordlessUser sign in", :type => :system do
     driven_by(:rack_test)
   end
 
-  it "displays error message if user's email not in system" do
-    visit "/passwordless_users/sign_in"
-  
-    expect(page.status_code).to be(200)
-    expect(page).to have_css("h2", text: "Log in")
+  context "non-existent user" do
+    it "displays error message if user's email not in system" do
+      visit "/passwordless_users/sign_in"
 
-    fill_in "Email", with: email
-    click_button "Log in"
+      expect(page.status_code).to be(200)
+      expect(page).to have_css("h2", text: "Log in")
 
-    expect(page).to have_css("h2", text: "Log in")
-    expect(page).to have_css("p.alert", text: "Could not find a user for that email address")
+      fill_in "Email", with: email
+      click_button "Log in"
+
+      expect(page).to have_css("h2", text: "Log in")
+      expect(page).to have_css("p.alert", text: "Could not find a user for that email address")
+    end
+
+    context "custom i18n" do
+      before do
+        I18n.backend.store_translations(:en, YAML.load(
+          <<~DEVISE_I18N
+          devise:
+            passwordless:
+              not_found_in_database: "Custom not found in database message"
+          DEVISE_I18N
+        ))
+      end
+
+      after do
+        I18n.backend.reload!
+      end
+
+      it "displays custom not found error message" do
+        visit "/passwordless_users/sign_in"
+
+        expect(page.status_code).to be(200)
+        expect(page).to have_css("h2", text: "Log in")
+
+        fill_in "Email", with: email
+        click_button "Log in"
+
+        expect(page).to have_css("h2", text: "Log in")
+        expect(page).to have_css("p.alert", text: "Custom not found in database message")
+      end
+    end
   end
 
   context "an existing user" do
@@ -24,13 +56,16 @@ RSpec.describe "PasswordlessUser sign in", :type => :system do
 
     it "sends magic link and successfully logs in when visiting magic link" do
       visit "/passwordless_users/sign_in"
-  
+
       expect(page.status_code).to be(200)
       expect(page).to have_css("h2", text: "Log in")
-  
+
       fill_in "Email", with: user.email
       click_button "Log in"
-  
+
+      # It displays a success message
+      expect(page).to have_text("A login link has been sent to your email address. Please follow the link to log in to your account.")
+
       # It sends a magic link email
       mail = ActionMailer::Base.deliveries.find {|x|
         x.to.include?(email)
@@ -45,6 +80,87 @@ RSpec.describe "PasswordlessUser sign in", :type => :system do
       # It successfully logs in
       expect(page).to have_css("h2", text: "Sign-in status")
       expect(page).to have_css("p.passwordless_user span.email", text: user.email)
+    end
+
+    context "custom i18n" do
+      after do
+        I18n.backend.reload!
+      end
+
+      context "using global i18n options" do
+        before do
+          I18n.backend.store_translations(:en, YAML.load(
+            <<~DEVISE_I18N
+            devise:
+              passwordless:
+                magic_link_sent: "Custom magic link sent message"
+              mailer:
+                magic_link:
+                  subject: "Custom magic link message"
+            DEVISE_I18N
+          ))
+        end
+
+        it "uses the correct i18n messages" do
+          visit "/passwordless_users/sign_in"
+
+          expect(page.status_code).to be(200)
+          expect(page).to have_css("h2", text: "Log in")
+
+          fill_in "Email", with: user.email
+          click_button "Log in"
+
+          # It displays a success message
+          expect(page).to have_text("Custom magic link sent message")
+
+          # It sends a magic link email
+          mail = ActionMailer::Base.deliveries.find {|x|
+            x.to.include?(email)
+          }
+          expect(mail.subject).to eq("Custom magic link message")
+        end
+      end
+
+      context "using resource-specific i18n options" do
+        before do
+          I18n.backend.store_translations(:en, YAML.load(
+            <<~DEVISE_I18N
+            devise:
+              passwordless:
+                passwordless_user:
+                  magic_link_sent: "Custom magic link sent message"
+                passwordless_confirmable_user:
+                  magic_link_sent: "YYY"
+                magic_link_sent: "XXX"
+              mailer:
+                magic_link:
+                  passwordless_user_subject: "Custom magic link message"
+                  passwordless_confirmable_user_subject: "YYY"
+                  subject: "XXX"
+            DEVISE_I18N
+          ))
+        end
+
+        it "uses the correct i18n messages" do
+          visit "/passwordless_users/sign_in"
+
+          expect(page.status_code).to be(200)
+          expect(page).to have_css("h2", text: "Log in")
+
+          fill_in "Email", with: user.email
+          click_button "Log in"
+
+          # It displays a success message
+          expect(page).to have_text("Custom magic link sent message")
+
+          # It sends a magic link email
+          mail = ActionMailer::Base.deliveries.find {|x|
+            x.to.include?(email)
+          }
+          expect(mail.subject).to eq("Custom magic link message")
+        end
+      end
+
     end
   end
 end

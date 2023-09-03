8956f6144a2644118e4c7b17caac7faacd1d41cd
Abe Voelker <_@abevoelker.com>
2023-09-03 17:14:06 -0500

    Use different tokenizer for PasswordlessConfirmableUser in dummy app spec

--- spec/dummy_app/app/models/passwordless_confirmable_user.rb
+++ spec/dummy_app/app/models/passwordless_confirmable_user.rb
@@ -3,4 +3,8 @@ class PasswordlessConfirmableUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :magic_link_authenticatable, :registerable,
          :confirmable, :rememberable, :validatable
+
+  def self.passwordless_tokenizer
+    "MessageEncryptorTokenizer"
+  end
 end
